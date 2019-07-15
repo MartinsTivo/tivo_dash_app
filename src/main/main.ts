5d3a78933b1d4e6a7ac1982701791eebd4b84a64
Martins Tiltins <martins.tiltins@tivohouses.com>
2019-07-15 16:29:52 +0300

    Auto updates

--- src/main/main.ts
+++ src/main/main.ts
@@ -44,4 +44,36 @@ app.on('activate', () => {
     }
 })
 
+if (isDev) {
+    console.log("Running in development")
+} else {
+    const server = 'https://github.com/MartinsTivo/tivo_dash_app.git';
+    const feed = `${server}/update/${process.platform}/${app.getVersion()}`
+
+    autoUpdater.setFeedURL({url: feed});
+
+    setInterval(() => {
+        autoUpdater.checkForUpdates()
+    }, 60000)
+
+    autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+        const dialogOpts = {
+          type: 'info',
+          buttons: ['Restart', 'Later'],
+          title: 'Application Update',
+          message: process.platform === 'win32' ? releaseNotes : releaseName,
+          detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+        }
+      
+        dialog.showMessageBox(dialogOpts, (response) => {
+          if (response === 0) autoUpdater.quitAndInstall()
+        })
+      })
+
+      autoUpdater.on('error', message => {
+        console.error('There was a problem updating the application')
+        console.error(message)
+      })
+}
+
 
